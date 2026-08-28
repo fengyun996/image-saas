a962d898007cb29af99c083436356212ea2f7793
zhuyongcheng <2234867425@qq.com>
2026-08-28 17:16:13 +0800

    # 插入可以不带自动创建的时间数据，虽然是禁止为空的

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createUserSchema } from '@/server/db/validate-schema';
+import { insertUserSchema } from '@/server/db/validate-schema';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const query = request.nextUrl.searchParams;
 
   try {
-    const result = createUserSchema.parse({
+    const result = insertUserSchema.parse({
       name: query.get('name'),
       email: query.get('email'),
     });
--- src/server/db/validate-schema.ts
+++ src/server/db/validate-schema.ts
@@ -1,5 +1,17 @@
-import { createInsertSchema } from 'drizzle-zod';
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 
 import { users } from './schema';
 
-export const createUserSchema = createInsertSchema(users);
+export const insertUserSchema = createInsertSchema(users, {
+  email: (schema) => schema.email(),
+  name: (schema) => schema.min(2),
+}); // 插入时存在createAt可以不传
+
+export const updateUserSchema = insertUserSchema.pick({
+  email: true,
+});
+
+export const queryUserSchema = createSelectSchema(users, {
+  email: (schema) => schema.email(),
+  name: (schema) => schema.min(2),
+}); // 请求时要传递createAt
