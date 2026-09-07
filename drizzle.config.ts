0afe03c0bf9ca84162b737e6fc89e5ad04638bf0
zhuyongcheng <2234867425@qq.com>
2026-09-07 17:40:44 +0800

    # 修复bug

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'drizzle-kit';
 import 'dotenv/config';
 export default defineConfig({
   schema: './src/server/db/schema.ts',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
     host: 'localhost',
     port: 5432,
     user: process.env.DATABASE_NAME!,
     password: process.env.DATABASE_PWD!,
     database: 'nextjs',
+    ssl: false,
   },
   verbose: true,
   strict: true,
