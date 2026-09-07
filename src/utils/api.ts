e21c25489a3dc42c743723c35434da4c0e4e9cc2
zhuyongcheng <2234867425@qq.com>
2026-09-07 17:13:07 +0800

    # 修复bug

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,21 +1,33 @@
-import { httpBatchLink, createTRPCClient } from "@trpc/client";
-import { createTRPCReact } from "@trpc/react-query";
-import type { AppRouter } from "@/server/router";
+import { httpBatchLink, createTRPCClient } from '@trpc/client';
+import { createTRPCReact } from '@trpc/react-query';
+import type { AppRouter } from '@/server/router';
 
 export const trpcClientReact = createTRPCReact<AppRouter>({});
 
 export const trpcClient = trpcClientReact.createClient({
-    links: [
-        httpBatchLink({
-            url: "http://localhost:3000/api/trpc",
-        }),
-    ],
+  links: [
+    httpBatchLink({
+      url: '/api/trpc',
+      fetch(url, options) {
+        return fetch(url, {
+          ...options,
+          credentials: 'include',
+        });
+      },
+    }),
+  ],
 });
 
 export const trpcPureClient = createTRPCClient<AppRouter>({
-    links: [
-        httpBatchLink({
-            url: "http://localhost:3000/api/trpc",
-        }),
-    ],
+  links: [
+    httpBatchLink({
+      url: '/api/trpc',
+      fetch(url, options) {
+        return fetch(url, {
+          ...options,
+          credentials: 'include',
+        });
+      },
+    }),
+  ],
 });
